ba6a90102a7485da683a20cd165f8c67f2204dfc
Clementin <clementin.descamps@gmail.com>
2021-03-26 16:22:09 +0100

    d

--- app/src/main/java/com/example/vivonexpo/exposantActivity.java
+++ app/src/main/java/com/example/vivonexpo/exposantActivity.java
@@ -13,5 +13,8 @@ public class exposantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exposant);
+
+
+                   
     }
 }
