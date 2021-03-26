02e31d777b33b6fafecb60fd693b69e655865410
Clementin <clementin.descamps@gmail.com>
2021-03-26 16:08:18 +0100

    exemple put extra

--- app/src/main/java/com/example/vivonexpo/staff.java
+++ app/src/main/java/com/example/vivonexpo/staff.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.content.Intent;
+
+
 public class staff extends AppCompatActivity {
 
 
@@ -13,5 +18,24 @@ public class staff extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff);
+
+
+
+        //Je recupere ma textview
+        final TextView textNomStaff = (TextView)findViewById(R.id.textNomStaff);
+        //je recupere le getExtra de mon intent
+        Intent intentResultat = getIntent();
+        String stringStaff = intentResultat.getStringExtra("staff");
+
+        //initialisation de mon objet de JSON
+        JSONObject objectStaff = null;
+        try {
+            //Je set mon objet JSON
+            objectStaff = new JSONObject(stringStaff);
+            textNomStaff.setText(objectStaff.getString("prenom") +" "+objectStaff.getString("nom"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
+
