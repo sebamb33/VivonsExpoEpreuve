cbb5d887554857494cb089ee7e120d31f1692d26
Clementin <clementin.descamps@gmail.com>
2021-03-26 15:45:57 +0100

    connexion

--- web/controleurs/authentication.php
+++ web/controleurs/authentication.php
@@ -4,7 +4,6 @@ require_once '../modeles/dao/utilisateurDAO.php';
 require_once '../modeles/dao/dBConnex.php';
 
 
-$_POST['login'] = 'staff';
-$_POST['mdp'] = '1234';
+
 
 print(json_encode(utilisateurDAO::authentification($_POST['login'], $_POST['mdp'])));
