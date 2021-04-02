2112457b68cb484f4952551039f7cd38b8415549
root <root@LAPTOP-OLKA7PUB.localdomain>
2021-04-02 13:21:28 +0200

    responsive-main

--- web/controleurs/authentication.php
+++ web/controleurs/authentication.php
@@ -4,6 +4,4 @@ require_once '../modeles/dao/utilisateurDAO.php';
 require_once '../modeles/dao/dBConnex.php';
 
 
-
-
 print(json_encode(utilisateurDAO::authentification($_POST['login'], $_POST['mdp'])));
